2f10a95c9f609c5beef6fcb49af2cb4e33587164
James Henderson <james@pcr.me.uk>
2013-08-19 23:01:03 +0100

    Adding deps

--- project.clj
+++ project.clj
@@ -1,7 +1,11 @@
-(defproject nomad-ring "0.1.0-SNAPSHOT"
+(defproject jarohen/nomad-ring "0.1.0-SNAPSHOT"
   :description "A plugin to start a Ring server via configuration in Nomad"
   :url "https://github.com/james-henderson/nomad-ring.git"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [leinjacker "0.4.1"]
+                 [jarohen/nomad "0.5.0"]]
 
   :eval-in-leiningen true)
