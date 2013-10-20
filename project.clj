0ce710b598df372a106b71095536a5aca460ec04
James Henderson <james@pcr.me.uk>
2013-10-20 17:21:06 +0100

    Releasing 0.2.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject jarohen/lein-frodo "0.2.1"
+(defproject jarohen/lein-frodo "0.2.2"
   :description "A Lein plugin to start an HTTP-kit server via configuration in Nomad"
   :url "https://github.com/james-henderson/lein-frodo.git"
   :license {:name "Eclipse Public License"
