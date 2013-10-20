7afc83c33293d4f1150a6398df621f3341dcbb7c
James Henderson <james@pcr.me.uk>
2013-10-20 17:10:59 +0100

    Correcting the URL in the project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject jarohen/lein-frodo "0.2.1"
   :description "A Lein plugin to start an HTTP-kit server via configuration in Nomad"
-  :url "https://github.com/james-henderson/nomad-ring.git"
+  :url "https://github.com/james-henderson/lein-frodo.git"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
