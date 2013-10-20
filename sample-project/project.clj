3c8f450e92ac3b1897eaf4809bfd2a19f5e5c936
James Henderson <james@pcr.me.uk>
2013-10-20 17:13:04 +0100

    Updating the sample project to use the stable version

--- sample-project/project.clj
+++ sample-project/project.clj
@@ -16,7 +16,7 @@
                  [org.clojure/clojurescript "0.0-1913"]
                  [org.clojure/tools.reader "0.7.8"]]
 
-  :plugins [[jarohen/lein-frodo "0.2.1-SNAPSHOT"]
+  :plugins [[jarohen/lein-frodo "0.2.1"]
             [lein-cljsbuild "0.3.3"]
             [lein-pdo "0.1.1"]]
 
