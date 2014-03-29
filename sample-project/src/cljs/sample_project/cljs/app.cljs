437990d276bd8aa90f2bdd2bf7ab1c250fb2d914
James Henderson <james@jarohen.me.uk>
2014-03-29 15:56:21 +0000

    Updating and simplifying the sample project

--- sample-project/src/cljs/sample_project/cljs/app.cljs
+++ sample-project/src/cljs/sample_project/cljs/app.cljs
@@ -1,26 +1,35 @@
 (ns sample-project.cljs.app
-    (:require [clojure.string :as s]
-              [sample-project.cljs.home :as home]
-              clojure.browser.repl)
-    (:require-macros [dommy.macros :refer [sel sel1]]))
+  (:require [dommy.core :as d]
+            [clojure.string :as s]
+            frodo.brepl)
+    (:require-macros [dommy.macros :refer [node sel1]]))
 
-(def default-hash "#/")
-
-(defn- bind-hash [hash-atom]
-  (letfn [(on-hash-change []
-            (reset! hash-atom (.-hash js/location)))]
-    
-    (set! (.-onhashchange js/window) on-hash-change)
-    
-    (when (s/blank? (.-hash js/location))
-      (set! (.-hash js/location) default-hash))
-    
-    (on-hash-change)))
+(defn code [s]
+  (node [:span {:style {:font-family "Courier New"}} s]))
 
 (set! (.-onload js/window)
       (fn []
-        (let [hash-atom (atom nil)]
-          (home/watch-hash! hash-atom)
-          (bind-hash hash-atom))))
+        (d/replace-contents! (sel1 :#content)
+                             (node [:div
+                                    [:h2 {:style {:margin-top :1em}}
+                                     "Hello world from Frodo!"]
+
+                                    [:h3 "Things to try:"]
+                                    
+                                    [:ul
+                                     [:li [:p "In your Clojure REPL (in the 'user' ns), run " (code "(reload-frodo!)") " to completely reload the webapp. The 'started at' time above should change."]]
+                                     [:li [:p "Connect to a CLJS bREPL by running " (code "(frodo-brepl)")]]
+                                     [:li
+                                      [:p "Once you've opened the bREPL, reload your browser to make the connection, then you can eval some CLJS."]
+                                      [:p "I recommend:"]
+                                      
+                                      [:ul
+                                       [:li (code "(js/alert \"Hello world!\")")]
+                                       [:li (code "(set! (.-backgroundColor js/document.body.style) \"green\")")]
+                                       [:li (code (s/join "\n" ["(in-ns 'sample-project.cljs.app)"
+                                                                "(d/replace-contents! (sel1 :#sample-div) (node [:strong \"Hi!\"]))"]))]]
+
+                                      [:p "Run " (code ":cljs/quit") " to get back to a Clojure REPL."]]]
+                                    [:div#sample-div]]))))
 
 
