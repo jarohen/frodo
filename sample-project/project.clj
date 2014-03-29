522e88b3f37557b7b8523c8eb01ecbbe9a35d649
James Henderson <james@jarohen.me.uk>
2014-03-29 12:06:46 +0000

    Deps in sample project

--- sample-project/project.clj
+++ sample-project/project.clj
@@ -5,21 +5,21 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
 
                  [ring/ring-core "1.2.0"]
                  [compojure "1.1.5"]
                  [hiccup "1.0.4"]
 
-                 [prismatic/dommy "0.1.1"]
+                 [prismatic/dommy "0.1.2"]
 
-                 [org.clojure/clojurescript "0.0-2156"]
+                 [org.clojure/clojurescript "0.0-2197"]
                  [org.clojure/tools.reader "0.8.3"]
-                 [jarohen/frodo-core "0.2.12-SNAPSHOT"]
+                 [jarohen/frodo-core "0.3.0-SNAPSHOT"]
                  [weasel "0.1.0"]]
 
-  :plugins [[jarohen/lein-frodo "0.2.12-SNAPSHOT"]
-            [lein-cljsbuild "1.0.0-alpha2"]
+  :plugins [[jarohen/lein-frodo "0.3.0-SNAPSHOT"]
+            [lein-cljsbuild "1.0.2"]
             [lein-pdo "0.1.1"]]
 
   :frodo/config-resource "sample-project-config.edn"
