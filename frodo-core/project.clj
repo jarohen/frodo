de9dbc8f4f9e127a1665f5932ad69a57b2fd68a7
James Henderson <james@jarohen.me.uk>
2014-04-03 20:41:11 +0100

    Deps in frodo-core

--- frodo-core/project.clj
+++ frodo-core/project.clj
@@ -3,11 +3,10 @@
   :url "https://github.com/james-henderson/frodo.git"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
                  [http-kit "2.1.18"]
                  [org.clojure/tools.nrepl "0.2.3"]
                  [org.clojure/tools.namespace "0.2.4"]
-                 [alembic "0.2.0"]
                  
                  [jarohen/nomad "0.6.3"]
 
