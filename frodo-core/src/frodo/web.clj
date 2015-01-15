6753ee79c7f931f2c2ce331fc1b74f63b5c6d80a
James Henderson <james@jarohen.me.uk>
2015-01-15 15:42:22 +0000

    Have to ensure that 'frodo-instance' is also accessible after a reload (yes, this is poor code - Phoenix does it a lot better)

--- frodo-core/src/frodo/web.clj
+++ frodo-core/src/frodo/web.clj
@@ -88,11 +88,13 @@
 (defn refresh-namespaces []
   (let [start-fn (some-> (resolve 'user/start-frodo) deref)
         stop-fn (some-> (resolve 'user/stop-frodo!) deref)
-        reload-fn (some-> (resolve 'user/reload-frodo!) deref)]
+        reload-fn (some-> (resolve 'user/reload-frodo!) deref)
+        instance-fn (some-> (resolve 'user/frodo-instance) deref)]
     (refresh)
     (intern 'user 'start-frodo! start-fn)
     (intern 'user 'stop-frodo! stop-fn)
-    (intern 'user 'reload-frodo! reload-fn)))
+    (intern 'user 'reload-frodo! reload-fn)
+    (intern 'user 'frodo-instance instance-fn)))
 
 (defn- reload-instance! [!instance config]
   (stop-instance! !instance)
